7f9d24126c2b5b7bfa6a9c9481268167025239fd
mstaskal <156384257+mstaskal@users.noreply.github.com>
2024-09-22 20:47:02 -0500

    Add files via upload
    Examples of PL/SQL Application programming including bulk processing of data to update tables and output table information

---
+++ Oracle_SQL/PLSQL_ApplicationProgramming.sql
@@ -0,0 +1,15 @@
+(ch4 assignment 4-8)
+To help track employee information, a new EMPLOYEE table is added to the database. Create a PL/SQL block to calculate annual raises and update employee salary amounts in the table. 
+	•Calculate 6% annual raises for all employees except the president.
+	•If a 6% raise totals more than $2,000, cap the raise at $2,000.
+	•Update the salary for each employee in the table.
+	•For each employee number, display the current annual salary, raise, and proposednew annual salary.
+	•Following the details for each employee, show the total cost of all employees’ salary increases for the company.
+
+
+
+
+(ch4 assignment 4-9)
+For each pledge payment from the donor, display the pledge ID, pledge amount, number of monthly payments, payment date, and payment amount. 
+The list should be sorted by pledge ID and thenby payment date. For the first payment made for each pledge, display “first payment” 
+on that output row.

c79fd2e5c264f65d39d37a1bfafe3e72994e001d
mstaskal <156384257+mstaskal@users.noreply.github.com>
2024-11-20 19:42:50 -0600

    Add files via upload

---
+++ Python/Football_Statistics_Dashboard/Dashboard_Code/ffb_ppr_dashboard_driver.py
@@ -0,0 +1,15 @@
+"""
+Name: Matt Staskal
+
+The purpose of this program is to create an instance of the PlayerCard Class in a Tkinter window, which will run
+the player information program
+"""
+
+
+from ffb_ppr_dashboard_class_definitions import *
+
+if __name__ == '__main__':
+    root = tk.Tk() # create Tkinter window
+    root.geometry("850x800")
+    base_card = PlayerCard(root) # instantiate instance of PlayerCard in window
+    root.mainloop()
